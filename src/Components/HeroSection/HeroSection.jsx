c42936964d01bf74ea14885f93c16944b2a89730
shaik3311 <162719241+shaik3311@users.noreply.github.com>
2026-02-10 10:00:17 +0530

    resume updated

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -54,8 +54,8 @@ const HeroSection = () => {
           </p>
 
           <div className="flex flex-col justify-center lg:justify-start items-center lg:items-start">
-            <a href="Shaik_Shashavali_Resume.pdf" 
-              download="My_Resume.pdf" 
+            <a href="My_Resume.pdf" 
+              download="Shaik_Shashavali_Resume.pdf" 
               className="mt-6 inline-flex items-center justify-center gap-2 px-6 py-3 w-fit rounded-lg 
              bg-blue-500 hover:bg-blue-600 transition-all duration-300 
              active:scale-95 hover:shadow-lg hover:shadow-blue-500/50">
